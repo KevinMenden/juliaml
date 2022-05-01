59931c06a171305c3ec30d8b5697f25a8ffafe1a
Kevin Menden <kevin.menden@t-online.de>
2022-05-01 09:40:23 +0200

    pca

--- PCA.jl
+++ PCA.jl
@@ -8,28 +8,24 @@ using MLJ
 using Plots
 import DataFrames
 
+
+function PCA(X::Matrix{<:Number}, n_components::Integer=2)
+    X_norm = X .- mean(X)
+    χ = cov(X_norm, dims=1)
+    evals = eigvals(χ)
+    evecs = eigvecs(χ)
+
+    sort_index = sortperm(evals, rev=true)
+    evecs = evecs[:, sort_index]
+    evecs_sub = evecs[:, 1:n_components]
+    transpose(transpose(evecs_sub) * transpose(X_norm))
+end
+
 # load data
 iris = load_iris()
 iris = DataFrames.DataFrame(iris)
 y, X = unpack(iris, ==(:target); rng=123);
 X = Matrix(X)
 
-# centering the data
-X_norm = X .- mean(X)
-
-# covariance
-χ = cov(X_norm, dims=1)
-
-# eigenvectors and values
-evals = eigvals(χ)
-evecs = eigvecs(χ)
-
-sort_index = sortperm(evals, rev=true)
-evecs = evecs[:, sort_index]
-
-n_components = 2
-evecs_sub = evecs[:, 1:n_components]
-
-x_transformed = transpose(transpose(evecs_sub) * transpose(X_norm))
-
+x_transformed = PCA(X)
 plot(x_transformed[:, 1], x_transformed[:, 2], seriestype=:scatter)
