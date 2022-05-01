801b3ed868468b1513dda4127d4f1563e696a27c
Kevin Menden <kevin.menden@t-online.de>
2022-05-01 09:30:22 +0200

    pca

--- PCA.jl
+++ PCA.jl
@@ -1,18 +1,35 @@
+
 """PCA implementation."""
 
 using LinearAlgebra
 using Random
 using Statistics
+using MLJ
+using Plots
+import DataFrames
 
-X = randn(20, 5)
+# load data
+iris = load_iris()
+iris = DataFrames.DataFrame(iris)
+y, X = unpack(iris, ==(:target); rng=123);
+X = Matrix(X)
+
+# centering the data
 X_norm = X .- mean(X)
 
-χ = cov(X_norm, dims=2)
+# covariance
+χ = cov(X_norm, dims=1)
 
+# eigenvectors and values
 evals = eigvals(χ)
 evecs = eigvecs(χ)
 
-
 sort_index = sortperm(evals, rev=true)
-evals = evals[sort_index]
+evecs = evecs[:, sort_index]
 
+n_components = 2
+evecs_sub = evecs[:, 1:n_components]
+
+x_transformed = transpose(transpose(evecs_sub) * transpose(X_norm))
+
+plot(x_transformed[:, 1], x_transformed[:, 2], seriestype=:scatter)
