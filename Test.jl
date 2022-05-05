ea99418ae1b8d4b95dd26bf36a8eaa33ace98ac8
kevinmenden <kevin.menden@t-online.de>
2022-05-05 21:14:11 +0200

    fix script

--- Test.jl
+++ Test.jl
@@ -7,13 +7,14 @@ using MLJ
 using Plots
 import DataFrames
 using MLDatasets
-
+using UMAP: umap
 
 train_x, train_y = MNIST.traindata()
 n_samples = 10000
-x_sub = x[:, 1:n_samples]
+x_sub = train_x[:,:, 1:n_samples]
 y_sub = train_y[1:n_samples]
-x_sub = convert(Matrix{Float32} ,transpose(x_sub))
+
+x_sub = convert(Matrix{Float32} ,transpose(reshape(x_sub, (784, n_samples))))
 
 # Test PCA
 x_pca = pca.PCA(x_sub)
@@ -22,4 +23,10 @@ scatter(
     x_pca[:, 2],
     marker_z = y_sub, 
     alpha=0.5)
+
+
+# Test UMAP
+x_umap = umap(transpose(x_sub); n_neighbors=10, min_dist=0.001, n_epochs=200)
+scatter(x_umap[1,:], x_umap[2,:], marker_z = y_sub)
+
 
