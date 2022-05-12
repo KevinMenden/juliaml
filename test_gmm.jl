d660d5286e96eff614483eca41631803321f66a2
kevinmenden <kevin.menden@t-online.de>
2022-05-12 20:27:12 +0200

    coninute with gmm

--- test_gmm.jl
+++ test_gmm.jl
@@ -1,13 +1,58 @@
 using Distributions
 using Plots
+using LinearAlgebra
 
-d1 = Normal(1, 0.2)
-d2 = Normal(3, 0.6)
-d3 = Normal(5, 0.5)
+# Create Distributions
 
-x1 = rand(d1, 200)
-x2 = rand(d2, 200)
-x3 = rand(d3, 200)
-x = vcat(x1, x2, x3)
+function create_sample(n::Int = 600)
+    n_per_dist = Integer(n / 3)
+    d1 = Normal(1, 0.2)
+    d2 = Normal(3, 0.6)
+    d3 = Normal(5, 0.5)
+    x1 = rand(d1, n_per_dist)
+    x2 = rand(d2, n_per_dist)
+    x3 = rand(d3, n_per_dist)
+    x = vcat(x1, x2, x3)    
+end
 
-histogram(x, bins=50)
+
+# GMM defintion
+mutable struct GMModel
+    n_components::Integer
+    μ::Vector{Number}
+    π::Vector{Number}
+    Σ::Vector{Number}
+
+    function GMModel(n_components::Integer)
+        μ = rand(n_components)
+        Σ = rand(n_components)
+        π = rand(Dirichlet(ones(n_components)))
+        new(n_components, μ, π, Σ)
+    end
+end
+
+function fitgmm(x::Vector{Number}, n_components::Integer, n_iter::Integer = 10)
+    gmm = GMModel(n_components)
+    n_samples = length(x)
+
+    for iter in 1:n_iter
+        # E-step
+        responsibilites = zeros((n_samples, n_components))
+        for s in 1:n_samples
+            for c in 1:n_components
+                d = Normal(gmm.μ[c], gmm.Σ[c])
+                responsibilites[s, c] = gmm.π[c] * pdf(d, x[s])
+            end
+        end
+        responsibilites = responsibilites ./ sum(responsibilites, dims=2)
+
+        gmm.π = sum(responsibilites, dims=1)
+
+        # M-Step
+
+    end
+end
+
+x = create_sample(600)
+fitgmm(x, 3)
+
