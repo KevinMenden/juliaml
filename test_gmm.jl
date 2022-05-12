ed126c0d8451d31af3d06d589c123c4b1bfcbbc7
Kevin Menden <kevin.menden@t-online.de>
2022-05-12 21:01:58 +0200

    working gmm implementation

--- test_gmm.jl
+++ test_gmm.jl
@@ -4,7 +4,7 @@ using LinearAlgebra
 
 # Create Distributions
 
-function create_sample(n::Int = 600)
+function create_sample(n::Int=600)
     n_per_dist = Integer(n / 3)
     d1 = Normal(1, 0.2)
     d2 = Normal(3, 0.6)
@@ -12,7 +12,7 @@ function create_sample(n::Int = 600)
     x1 = rand(d1, n_per_dist)
     x2 = rand(d2, n_per_dist)
     x3 = rand(d3, n_per_dist)
-    x = vcat(x1, x2, x3)    
+    x = vcat(x1, x2, x3)
 end
 
 
@@ -31,7 +31,7 @@ mutable struct GMModel
     end
 end
 
-function fitgmm(x::Vector{Float64}, n_components::Integer, n_iter::Integer = 10)
+function fitgmm(x::Vector{Float64}, n_components::Integer, n_iter::Integer=10)
     gmm = GMModel(n_components)
     n_samples = length(x)
 
@@ -46,13 +46,20 @@ function fitgmm(x::Vector{Float64}, n_components::Integer, n_iter::Integer = 10)
         end
         responsibilites = responsibilites ./ sum(responsibilites, dims=2)
 
-        gmm.π = sum(responsibilites, dims=1)
+        c_resp = vec(sum(responsibilites, dims=1))
 
         # M-Step
-
+        for c in 1:n_components
+            #gmm.μ[c] = sum(responsibilites[:, c] .* x) / n_samples
+            #gmm.Σ[c] = sum(responsibilites[:, c] .* (x .- gmm.μ[c]) .^ 2) / n_samples
+            gmm.μ[c] = sum(responsibilites[:, c] .* x) / c_resp[c]
+            gmm.Σ[c] = sqrt(sum(responsibilites[:, c] .* (x .- gmm.μ[c]) .^ 2) / c_resp[c])
+            gmm.π[c] = c_resp[c] ./ n_samples
+        end
     end
+    gmm
 end
 
 x = create_sample(600)
-fitgmm(x, 3)
+gmm = fitgmm(x, 3, 50)
 
